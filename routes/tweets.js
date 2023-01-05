c3b1da8a5debe6f0418905b16a8363c10cd1a601
Anya Mittal <anya@HUAWEI_Y6_2018-2f11a7496e.localdomain>
2023-01-05 16:02:26 -0500

    bug fixes

--- routes/tweets.js
+++ routes/tweets.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const passport = require('passport');
 const Tweet = require('../models/tweets')
 
-router.post('/', passport.authenticate('jwt', { session: false }), async ({body}, res) => {
+router.post('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const tweet = new Tweet();
-    const user = req.user();
+    const user = req.user;
 
     tweet.author = user;
-    tweet.content = body.content;
+    tweet.content = req.body.content;
 
     await tweet.save();
     return res.json(tweet.toJSON());
